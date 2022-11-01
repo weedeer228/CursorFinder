533a14242289fcfe15a06f4d5f4bebd53ba5e480
nikita_228 <poletaevn@protonmail.com>
2022-11-01 17:32:56 +0300

    Added new field for CursorPosition

--- CursorFinderHost/Migrations/202210241140433_Init.cs
+++ CursorFinderHost/Migrations/202210241140433_Init.cs
@@ -7,23 +7,23 @@
     {
         public override void Up()
         {
-            CreateTable(
-                "dbo.CursorPositions",
-                c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        XPos = c.Int(nullable: false),
-                        YPos = c.Int(nullable: false),
-                        DateTime = c.DateTime(nullable: false),
-                        ActionType = c.Int(nullable: false),
-                    })
-                .PrimaryKey(t => t.Id);
+            //CreateTable(
+            //    "dbo.CursorPositions",
+            //    c => new
+            //        {
+            //            Id = c.Int(nullable: false, identity: true),
+            //            XPos = c.Int(nullable: false),
+            //            YPos = c.Int(nullable: false),
+            //            DateTime = c.DateTime(nullable: false),
+            //            ActionType = c.Int(nullable: false),
+            //        })
+            //    .PrimaryKey(t => t.Id);
             
         }
         
         public override void Down()
         {
-            DropTable("dbo.CursorPositions");
+            //DropTable("dbo.CursorPositions");
         }
     }
 }
---
+++ CursorFinderHost/Migrations/202211011431345_UserTokenField.cs
@@ -0,0 +1,18 @@
+﻿namespace CursorFinder.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UserTokenField : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.CursorPositions", "UserToken", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.CursorPositions", "UserToken");
+        }
+    }
+}
